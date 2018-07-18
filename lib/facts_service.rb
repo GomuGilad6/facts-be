264f8b2c77a880a9185ff417652d771fe5d054b2
Oded Magger <oded.magger@wework.com>
2018-07-18 14:36:48 +0300

    comments are important

--- lib/facts_service.rb
+++ lib/facts_service.rb
@@ -8,7 +8,7 @@ class FactsService
   private
 
   def generate_single_random_fact
-    # TODO IMPLEMENT
+    # TODO IMPLEMENT - will fill this with facts once we have a concrete fact.
     Fact.new(fact: 'test', type: 'testfact')
   end
 end
