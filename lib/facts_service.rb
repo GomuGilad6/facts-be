4322bf152f67acf8d247ade80ddf8e6c6d41ce41
Oded Magger <oded.magger@wework.com>
2018-07-18 12:31:35 +0300

    FactsService basic stub

--- lib/facts_service.rb
+++ lib/facts_service.rb
@@ -1,6 +1,12 @@
 class FactsService
   def generate_facts(amount)
+    Array.new(amount) { generate_single_random_fact }
+  end
+
+  private
+
+  def generate_single_random_fact
     # TODO IMPLEMENT
-    # Responsible for creating diverse facts ready to be sent by the controller
+    Fact.new(fact: 'test', type: 'testfact')
   end
 end
