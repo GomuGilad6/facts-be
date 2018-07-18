c7ec2c1b0c6a41d2a7f41074a8fe1c6ada91840e
Oded Magger <oded.magger@wework.com>
2018-07-18 16:46:21 +0300

    Render an array of facts to JSON!

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -2,19 +2,8 @@
 
 class FactsController < ApplicationController
   def index
-    # TODO: IMPLEMENT
-    # Possibly use ActiveSerializer
-    # Returns
-    # {
-    #   [
-    #     {
-    #       type: '',
-    #       fact: '',
-    #       image: '' / nil
-    #     },
-    #     {
-    #     }
-    #   ]
-    # }
+    facts = FactsService.new.generate_facts(3)
+    facts_array = facts.map { |fact| { type: fact.type, fact: fact.fact, image: fact.image } } # TODO-OM Use Activemodel serializer
+    render json: facts_array
   end
 end
