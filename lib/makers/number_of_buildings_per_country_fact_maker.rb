09aa8c7081a93fd12ceb9da09ba46533d49f09dd
Oded Magger <oded.magger@wework.com>
2018-07-19 13:31:27 +0300

    rephrasing

--- lib/makers/number_of_buildings_per_country_fact_maker.rb
+++ lib/makers/number_of_buildings_per_country_fact_maker.rb
@@ -5,7 +5,7 @@ class NumberOfBuildingsPerCountryFactMaker < BaseFactMaker
     index = rand(countries.size - 1)
     num_of_buildings = countries[index][:markets][0][:buildings].size
     country_name = countries[index][:name]
-    fact = "There are #{num_of_buildings} buildings in #{country_name}"
+    fact = "WeWork has #{num_of_buildings} locations in #{country_name}"
     Fact.new(fact: fact, type: FACT)
   end
 end
