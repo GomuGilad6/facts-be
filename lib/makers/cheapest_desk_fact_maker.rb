449bf008895772521160f5f40958be44548254b2
Oded Magger <oded.magger@wework.com>
2018-07-19 14:27:13 +0300

    bug fix - cheapest no always have price

--- lib/makers/cheapest_desk_fact_maker.rb
+++ lib/makers/cheapest_desk_fact_maker.rb
@@ -2,11 +2,15 @@ class CheapestDeskFactMaker < BaseFactMaker
   def make
     groupings = LocationsService.call_api('geogroupings')
     groupings_buildings = groupings['buildings']
-    building_entry = groupings_buildings.sample
-    building = LocationsService.call_api("buildings/#{building_entry['id']}")['building']
 
-    building_name = building['name']
-    min_desk_price = building['min_desk_price']
+    min_desk_price = nil
+    while min_desk_price.nil?
+      building_entry = groupings_buildings.sample
+      building = LocationsService.call_api("buildings/#{building_entry['id']}")['building']
+
+      building_name = building['name']
+      min_desk_price = building['min_desk_price']
+    end
 
     fact_text = "At #{building_name}, you can get a desk for as cheap as #{min_desk_price} !"
 
