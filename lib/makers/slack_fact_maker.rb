119aed9174838ba19fd071d0a10bbf70a11b6aa6
AdiSaranga <adi.saranga@wework.com>
2018-07-19 14:29:37 +0300

    remove line break

--- lib/makers/slack_fact_maker.rb
+++ lib/makers/slack_fact_maker.rb
@@ -1,7 +1,7 @@
 class SlackFactMaker < BaseFactMaker
   FACTS = [
     Fact.new(fact: '“It’s not a bug, it’s a Fisher” (reference to @Isaac Fisher)', type: SLACK),
-    Fact.new(fact: '“@Ron Nahshon "@Dovrat Gilboa didn\'t even want to join!" \n @Dovrat Gilboa "Snitches get stitches"', type: SLACK),
+    Fact.new(fact: '“@Ron Nahshon "@Dovrat Gilboa didn\'t even want to join!" @Dovrat Gilboa "Snitches get stitches"', type: SLACK),
     Fact.new(fact: '@Tomer Desser “Dresser not like שידה, Dresser like מתלבש”', type: SLACK),
     Fact.new(fact: '@Avishai Lippner about @Limor Mekaiten: Stack Overflow was her only friend...', type: SLACK)].freeze
   def make
