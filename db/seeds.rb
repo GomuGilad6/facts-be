8ff3f25f11337d721e5f06c52c6b74f1477d7b11
AdiSaranga <adi.saranga@wework.com>
2018-07-19 14:09:35 +0300

    fix image url

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,6 @@ CuratedFact.create(fact_desc: 'Over 16 million square feet space WeWork Lease Gl
 CuratedFact.create(fact_desc: 'WeWork Tower 49 in New York City has nearly 4,500 members')
 CuratedFact.create(fact_desc: 'Adam Neumann has 5 kids', image_url: 'https://images.successstory.com/adminimg/image_uploads/content_images/adam-neumann-story_1460974437.jpg')
 CuratedFact.create(fact_desc: 'WeWork TLV is the best')
-CuratedFact.create(fact_desc: 'WeWork Goes Vegetarian', image_url: 'https://www.google.co.il/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjNuJ7zgKvcAhXOY1AKHbEODN8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.nhs.uk%2Flive-well%2Feat-well%2Fthe-vegetarian-diet%2F&psig=AOvVaw1U81Y_ybjz1t8QVJaCeTdD&ust=1532084295804123')
-CuratedFact.create(fact_desc: 'Miguel began his entrepreneurial career by co-founding the company - English, Baby!', image_url: 'https://www.google.co.il/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwjJsvblgKvcAhUPblAKHe2XCOQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.inc.com%2Fzoe-henry%2Fhow-miguel-mckelvey-went-from-handyman-to-unicorn-leader.html&psig=AOvVaw2ZWgOl8etRDu-3-nL_008b&ust=1532084262389922')
+CuratedFact.create(fact_desc: 'WeWork Goes Vegetarian', image_url: 'https://assets.nhs.uk/prod/images/T_0318_vegetarian-meal-guide_916.2e16d0ba.fill-1840x1226.jpg')
+CuratedFact.create(fact_desc: 'Miguel began his entrepreneurial career by co-founding the company - English, Baby!', image_url: 'https://www.incimages.com/uploaded_files/image/970x450/getty_456946680_68528.jpg')
 CuratedFact.create(fact_desc: 'Aviv Geffen chooses the songs we hear', image_url: 'https://scontent.fhfa2-1.fna.fbcdn.net/v/t1.0-9/6237_533679606660552_37098859_n.jpg?_nc_cat=0&oh=e5c572013861df4dcdaafa68f6136719&oe=5BE6EDB4')
