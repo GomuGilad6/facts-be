59564651965fa3e3f5b74a2ac74068868702f4f5
Oded Magger <oded.magger@wework.com>
2018-07-19 13:51:30 +0300

    Bug fix

--- lib/makers/number_of_buildings_per_country_fact_maker.rb
+++ lib/makers/number_of_buildings_per_country_fact_maker.rb
@@ -2,9 +2,10 @@ class NumberOfBuildingsPerCountryFactMaker < BaseFactMaker
   def make
     countries_payload = LocationsService.call_api('countries')
     countries = countries_payload.with_indifferent_access[:countries]
-    index = rand(countries.size - 1)
-    num_of_buildings = countries[index][:markets][0][:buildings].size
-    country_name = countries[index][:name]
+    country = countries.sample
+    markets = country[:markets]
+    num_of_buildings = markets.sum { |m| m['buildings'].count }
+    country_name = country[:name]
     fact = "WeWork has #{num_of_buildings} locations in #{country_name}"
     Fact.new(fact: fact, type: FACT)
   end
